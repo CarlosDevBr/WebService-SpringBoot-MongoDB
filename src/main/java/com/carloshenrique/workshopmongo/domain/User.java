7f8491abd1d0a4b6c5a0092e3bca7523d57940ff
cahzerohenrique@gmail.com <CarlosDevBr>
2022-03-06 20:38:30 -0300

    Referenciando os posts do usuario

--- src/main/java/com/carloshenrique/workshopmongo/domain/User.java
+++ src/main/java/com/carloshenrique/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.carloshenrique.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -13,6 +16,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -56,6 +62,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -71,6 +85,5 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-	
+	}	
 }
